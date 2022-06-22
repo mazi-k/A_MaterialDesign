1b5d3d6c68764db31cdda383c20cd6168699acbf
maria <podliza3000@gmail.com>
2022-06-22 18:20:01 +0300

    replace images with fragments

--- app/src/main/java/com/example/a_materialdesign/view/api/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/a_materialdesign/view/api/ViewPagerAdapter.kt
@@ -1,27 +1,27 @@
 package com.example.a_materialdesign.view.api
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 private const val ADAPTER_SIZE = 3
+private const val EARTH_FRAGMENT = 0
+private const val MARS_FRAGMENT = 1
+private const val SYSTEM_FRAGMENT = 2
 
-class ViewPagerAdapter(fragmentManager: FragmentManager) :
-    FragmentStatePagerAdapter(fragmentManager) {
-    override fun getCount(): Int {
+class ViewPagerAdapter(fragmentManager: FragmentActivity) :
+    FragmentStateAdapter(fragmentManager) {
+
+    override fun getItemCount(): Int {
         return ADAPTER_SIZE
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
-            BaseFragment.EARTH_FRAGMENT -> "Earth"
-            BaseFragment.MARS_FRAGMENT -> "Mars"
-            BaseFragment.SYSTEM_FRAGMENT -> "System"
-            else -> "Earth"
+            EARTH_FRAGMENT -> EarthFragment()
+            MARS_FRAGMENT -> MarsFragment()
+            SYSTEM_FRAGMENT -> SystemFragment()
+            else -> EarthFragment()
         }
-    }
-
-    override fun getItem(position: Int): Fragment {
-        return BaseFragment.newInstance(position)
     }
 }
