00bcefb3e541c0ff3ccfa393d1eb35b4ba66a0e6
maria <podliza3000@gmail.com>
2022-07-01 13:21:52 +0300

    add animation on planet pictures

--- app/src/main/java/com/example/a_materialdesign/view/api/EarthFragment.kt
+++ app/src/main/java/com/example/a_materialdesign/view/api/EarthFragment.kt
@@ -1,19 +1,51 @@
 package com.example.a_materialdesign.view.api
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.a_materialdesign.R
+import com.example.a_materialdesign.databinding.FragmentEarthBinding
 
 class EarthFragment : Fragment() {
+
+    val duration = 2000L
+    private var isAnimate = false
+
+    private var _binding: FragmentEarthBinding? = null
+    private val binding: FragmentEarthBinding
+        get() {
+            return _binding!!
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_earth, container, false)
+        _binding = FragmentEarthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        imageAnimate()
+    }
+
+    private fun imageAnimate() {
+        binding.imageView.setOnClickListener {
+            isAnimate = !isAnimate
+            if (isAnimate){
+                ObjectAnimator.ofFloat(binding.imageView, View.ROTATION, 360f).setDuration(duration)
+                    .start()
+            } else {
+                ObjectAnimator.ofFloat(binding.imageView, View.ROTATION, -360f).setDuration(duration)
+                    .start()
+            }
+
+        }
+
     }
 }
