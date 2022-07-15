e3439ba3ef1767c32dc8b83c57c7d917cb42d84c
maria <podliza3000@gmail.com>
2022-07-15 15:23:43 +0300

    add spans

--- app/src/main/java/com/example/a_materialdesign/view/settings/AboutAppFragment.kt
+++ app/src/main/java/com/example/a_materialdesign/view/settings/AboutAppFragment.kt
@@ -2,10 +2,15 @@ package com.example.a_materialdesign.view.settings
 
 import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BulletSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.a_materialdesign.R
 import com.example.a_materialdesign.databinding.FragmentAboutAppBinding
 
 class AboutAppFragment: Fragment() {
@@ -29,7 +34,36 @@ class AboutAppFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         scrollFade()
+        textBullet()
     }
+
+    private fun textBullet() {
+        val text = requireActivity().resources.getString(R.string.large_text)
+        val spannableString = SpannableString(text)
+        val list = text.indexesOf("\n")
+        var current = 0
+        list?.forEach {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                spannableString.setSpan(
+                    BulletSpan(
+                        20,
+                        ContextCompat.getColor(requireContext(), R.color.colorAccent),
+                        5
+                    ), current, it, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+
+                binding.largeText.text = spannableString
+            }
+            current = it + 1
+        }
+
+    }
+
+    private fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int>? =
+        this?.let {
+            (if (ignoreCase) Regex(substr, RegexOption.IGNORE_CASE) else Regex(substr))
+                .findAll(it).map { it.range.first }.toList()
+        }
 
     private fun scrollFade() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -37,7 +71,6 @@ class AboutAppFragment: Fragment() {
                 binding.header.isSelected = binding.scrollView.canScrollVertically(-1)
             }
         }
-        //не работает
     }
 
     override fun onDestroy() {
